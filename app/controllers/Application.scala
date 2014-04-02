2981b59cd79d99b66f87711006a64ede2b2ab8f4
lindseydew <lindsey.dew@guardian.co.uk>
2014-04-02 23:24:15 +0100

    implement oauth login via github

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,6 +5,14 @@ import lib._
 import play.api.Logger
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
+import play.api.libs.ws.WS._
+import play.api.libs.ws.WS
+import play.api.libs.json.{JsString, JsUndefined, JsSuccess, Json}
+import org.kohsuke.github.{GitHub, GHIssueState}
+import scala.util.matching.Regex
+import play.api.libs.oauth.{OAuth, ServiceInfo, ConsumerKey}
+import java.net.URLEncoder
+
 
 object Application extends Controller {
 
@@ -27,4 +35,36 @@ object Application extends Controller {
     } else future { NotAcceptable }
   }
 
+  def index = Action {
+    Ok(views.html.index())
+  }
+
+  def signin = Action {
+    val githubUrl = GithubAuthUri.uri
+    Redirect(githubUrl)
+  }
+
+
+  def callback(code: String, state: String) = Action.async {
+
+    val githubUrl = new GithubAccessTokenUri(code, state).uri
+
+    WS.url(githubUrl).withHeaders(("Accept", "application/json")).post("").map { res =>
+      Json.parse(res.body) \ "access_token" match {
+        case JsString(accessCode) => Redirect("/home").withSession("userId" -> accessCode)
+        case _ => {
+          Logger.error("couldn't parse the access token from the response")
+          //todo - send a message back to user
+          Redirect("/")
+        }
+      }
+    }
+  }
+
+  def home = Action { req =>
+    req.session.get("userId") match {
+      case Some(accessToken) => Ok(views.html.home())
+      case None => Ok(views.html.index())
+    }
+  }
 }
