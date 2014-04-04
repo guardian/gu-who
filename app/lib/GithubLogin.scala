a5e4b24b184b3757e6b44f461b70ab751f99384e
lindseydew <lindsey.dew@guardian.co.uk>
2014-04-04 15:40:34 +0100

    set client secret and id as environment variables and tidy the code

--- app/lib/GithubLogin.scala
+++ app/lib/GithubLogin.scala
@@ -2,29 +2,16 @@ package lib
 
 import java.net.URLEncoder
 
-trait GithubLogin {
-    val baseUrl: String
-    val queryStringParams: Map[String, String]
-    val clientId = "312fb1f543eff4c2a69c"
-    val clientSecret = "26a2a723f0c4482ff4c562c29bdd973f483768fd"
-    val scope = "read:org,write:org"
-    val state = "ASUPERRANDOMSTRING"
+object GithubAppConfig {
 
-    lazy val queryString: String = {
-      queryStringParams.map {
-        case (k, v) => k + "=" + URLEncoder.encode(v, "UTF-8")
-      }.mkString("&")
-    }
+  import play.api.Play.current
+  val config = play.api.Play.configuration
 
-    lazy val uri = baseUrl + "?" + queryString
+  val clientId = config.getString("securesocial.github.clientId").getOrElse("blah")
+  val clientSecret = config.getString("securesocial.github.clientSecret").getOrElse("blah")
+  val scope = "read:org,write:org"
+  val authUrl = "https://github.com/login/oauth/authorize"
+  val accessTokenUrl = "https://github.com/login/oauth/access_token"
+
 }
 
-object GithubAuthUri extends GithubLogin {
-  val queryStringParams = Map("client_id" -> clientId, "state" -> state, "scope" -> scope)
-  val baseUrl = "https://github.com/login/oauth/authorize"
-}
-
-class GithubAccessTokenUri(code: String, state: String) extends GithubLogin {
-  val queryStringParams = Map("code" -> code, "client_id" -> clientId, "client_secret" -> clientSecret)
-  val baseUrl = "https://github.com/login/oauth/access_token"
-}
